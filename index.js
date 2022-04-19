22e03f07fc3cbd7ca2c164181df2d956c9bec9a0
Hector del Reguero Valle <hector_valle@live.com.mx>
2022-04-18 23:29:48 -0500

    Aqui utilice las directivas para tener componenetes reactivos y poder agregar una imagen

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-Vue.createApp({
+const app = Vue.createApp({
     data(){
         return{
-            text: "hola mundo"
+            img: "https://yt3.ggpht.com/ytc/AKedOLQMSrgxItPwy1gW4nke8tyEXNImWjwt3upFTg7g=s900-c-k-c0x00ffffff-no-rj"
         }
-    }
+    },
+    template: `<img v-bind:src="img" v-bind:alt="img">`
 }).mount("#app");
